99a031dcc0e809779911c3e46a24d2ff9c348321
Patryk Drozdz <patrol687@wp.pl>
2016-05-16 02:06:06 +0200

    second

--- AR1/Source.cpp
+++ AR1/Source.cpp
@@ -1,15 +1,16 @@
 #include<mpi.h>
 #include<iostream>
 #include<cstdlib>
-#include<vector>
 #include<sstream>
+#include<Windows.h>
 
 #define N 10
+#define s 4 //ziaron psudolosowoœci
 
 using namespace std;
 
 ////////////////////////////////////////////
-bool Randed(int number, double tab[], int count) {
+bool Randed(int number, int tab[], int count) {
 	
 	int i = 0;
 
@@ -26,40 +27,49 @@ bool Randed(int number, double tab[], int count) {
 		return false;
 	}
 }
+//silnia
+double fact(int n) {
+	double res;
+	for (int i = 1; i <=n; i++) {
+		res = res*i;
+	}
+	return res;
+}
+
+//suma
+double Sum(int a) {
+	double sum;
+	for (int i = 0; i <= a; i++) {
+		sum += i;
+	}
+	return sum;
+}
+
 //////////////////////////////////////////////////
 int main(int argc, char* argv[]) {
 
 	MPI::Init();
-	int rank, size, cycles, rest;
+	int rank, size; 
 	int tag = 0;
 	int proc = 1;
 	int Tmax = 100;//maksymalny rozmiar zadania
-	int s;//ziaron psudolosowoœci
 	int randMin = 1;//dolna granica rand
 	int randMax = Tmax;//górna granica rand
 	rank = MPI::COMM_WORLD.Get_rank();
 	size = MPI::COMM_WORLD.Get_size();
 	int randNumber = 0;
 	int randed = 0;
-	stringstream zs;
-	//MPI_Status status;
-
-	zs << argv[0];
-
-	zs >> s;
-
-
-	//dekompozycja
-	///////////////////////////////////////
-	cycles = N / (size - 1);//cykle do wykonania(-1, bo bez procesu 0-master)
-	rest = N%(size - 1);
-	/////////////////////////////////////
+	MPI_Status status;
 
 	//tablice
 	/////////////////////////////////////////
-	double* Rands = new double[N];//wylosowane liczby
-	double* Ttab = new double[N];//odebrane
+	int* Rands = new int[N];//wylosowane liczby
+	int* Ttab = new int[N];//odebrane
 	int* Tags = new int[N];
+	int* Ranks = new int[N];
+
+	double* Relse = new double[N];
+	double* RM = new double[N];
 
 	////////////////////////////////////////
 
@@ -85,23 +95,36 @@ int main(int argc, char* argv[]) {
 	} 
 
 	//rozdzielanie wysy³anie
-	//for (int i = 0; i < N; i++) {
-	//	MPI_Send(&Rands[i], 1, MPI_DOUBLE, proc, tag, MPI_COMM_WORLD);
-	//	Tags[i] = proc;
-	//	proc++;
-	//	tag++;
-	//	if (proc == size) {
-	//		proc = 0;
-	//	}
-	//}
-	//tag = 0;
-	//proc = 0;
+	for (int i = 0; i < N; i++) {
+		MPI_Send(&Rands[i], 1, MPI_INT, proc, tag, MPI_COMM_WORLD);
+		Tags[i] = tag;
+		Ranks[i] = proc;
+		proc++;
+		tag++;
+		if (proc == size) {
+			proc = 0;
+		}
+	}
+	tag = 0;
+	proc = 0;
 
-	////odbieranie
-	//for (int i = 0; i < N; i++) {
-	//	MPI_Recv(&Ttab[i], 1, MPI_DOUBLE, 0,tag, MPI_COMM_WORLD, &status);
-	//	tag++;
-	//}
+	//odbieranie
+	for (int i = 0; i < N; i++) {
+		MPI_Recv(&Ttab[i], 1, MPI_INT, 0,tag, MPI_COMM_WORLD, &status);
+		tag++;
+	}
+
+	if (rank != 0) {
+
+		for (int i = 0; i < N; i++) {
+			Relse[i] = (double)fact(Ttab[i]) / (double)Sum(Ttab[i]);
+			Sleep(Ttab[i]);
+			if (Relse[i] != NULL) {
+				MPI_Send(&Relse[i], 1, MPI_DOUBLE, 0, tag, MPI_COMM_WORLD);
+			}
+		}
+
+	}
 
 	cout << endl;
 	cout << endl;
